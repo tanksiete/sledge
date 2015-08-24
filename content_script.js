cc0af52a3639dc75a7ab6f2b87f47704abb66748
kleaver <kleaver@users.noreply.github.com>
2015-08-23 21:18:44 -0500

    created file browser, added styling

--- content_script.js
+++ content_script.js
@@ -1,7 +1,33 @@
 console.log('applying sledge...');
 
-$files = $('#files .file');
+$fileDiffs = $('#files .file');
 
-$files.each(function() {
+$body = $('body');
+
+createFileDiffBrowser($body, $fileDiffs);
+
+$fileDiffs.each(function() {
   $(this).hide();
 });
+
+function createFileDiffBrowser(body, fileDiffs) {
+  var fileBrowserHtml = '<div id="sledge-file-browser">';
+
+  fileBrowserHtml += '<ul>';
+
+  fileDiffs.each(function(i) {
+    fileBrowserHtml += '<li class="sledge-menu-item" id="sledge-file-browser-diff-' + i + '">' + extractFileDiffTitle($(this)) + '</li>';
+  });
+
+  fileBrowserHtml += '</ul></div>';
+  $fileBrowserHtml = body.append(fileBrowserHtml).find('li.sledge-menu-item').each(function(i) {
+    $(this).click(function(e) {
+      $('#sledge-file-browser-diff-' + i).toggleClass('selected');
+      $('#diff-' + i).toggle();
+    })
+  });
+
+  function extractFileDiffTitle(fileDiff) {
+    return fileDiff.find('.file-header .file-info .js-selectable-text').attr('title');
+  }
+}
